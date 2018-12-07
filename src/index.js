e42231e326b37e3149ae9411d6f2f9fbfd3089a3
Nath Paiva <hello@nathpaiva.com.br>
2018-12-06 22:25:01 -0200

    Create a server db

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require('dotenv').config({ path: '.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.start(
+  {
+    cors: {
+      credentials:  true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log('------------------------------------------------------------');
+    console.log('------------------------------------------------------------');
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+    console.log('------------------------------------------------------------');
+    console.log('------------------------------------------------------------');
+  }
+);
