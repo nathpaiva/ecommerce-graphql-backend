d82507e545adbe1454e985821b80737375626c78
Nath Paiva <hello@nathpaiva.com.br>
2018-12-08 21:07:58 -0200

    Create query and mutation for items

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,19 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, context, info) {
+		console.log("​createDog -> args", args)
+    return args;
+  },
+
+  async createItem(parent, args, context, info) {
+
+    const item = await context.db.mutation.createItem({
+      data : {
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+const Query = {
+  dogs(parent, args, context, info) {
+    // console.log("global", global);
+    return [{ name: 'Tag' }, { name: 'Layla' }]
+  },
+
+  async items(parent, args, context, info) {
+    const items = await context.db.query.items();
+
+    return items;
+  }
+};
 
 module.exports = Query;
